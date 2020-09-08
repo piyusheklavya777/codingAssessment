537839618206894ccb3f8d12b42e4c2761fad8b7
Eklavya <Piyush.Eklavya@team.telstra.com>
2020-09-08 10:34:57 +0530

    front-end

--- frontend/public/js/services/fetchAndShowData.js
+++ frontend/public/js/services/fetchAndShowData.js
@@ -1,25 +1,49 @@
-export function fetchAndShowData() {
-    // const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    // const url = `https://localhost:3000/products`
-    fetch("localhost:4201/articles/")
-        .then(function (response) {
-        return response.json();
-    })
-        .then(function (dataArray) {
-        console.log(dataArray);
-        var tbody = document.querySelector('tbody');
-        // go through workshops array and form HTML within tbody
-        dataArray.forEach(function (dataEach) {
-            // tbody.innerHTML += `<tr>
-            //     <td>
-            //         <img src="${dataEach.imageUrl}" style="width: 200px;" />
-            //     </td>
-            //     <td>${dataEach.name}</td>
-            //     <td>${dataEach.location.city}</td>
-            // </tr>`;
-        });
-    })
-        .catch(function (error) {
-        console.log(error.message);
+var datatrial = require('.../data.json');
+// export function fetchAndShowData() {
+//    // const proxyurl = "https://cors-anywhere.herokuapp.com/";
+//    // const url = `https://localhost:3000/products`
+//      //fetch( `localhost:4201/articles/` )
+//     fetch(datatrial)
+//         .then(function( response : Response ) {
+//             return response.json();
+//         })
+//         .then(function( dataArray : datainterface[] ) {
+//             console.log( dataArray);
+//             const tbody = <HTMLElement> document.querySelector( 'tbody' );
+//             // go through workshops array and form HTML within tbody
+//             dataArray.forEach(function( dataEach ) {
+//                 // tbody.innerHTML += `<tr>
+//                 //     <td>
+//                 //         <img src="${dataEach.imageUrl}" style="width: 200px;" />
+//                 //     </td>
+//                 //     <td>${dataEach.name}</td>
+//                 //     <td>${dataEach.location.city}</td>
+//                 // </tr>`;
+//             });
+//         })
+//         .catch(function( error ) {
+//             console.log( error.message );
+//         });
+// }
+export function fetchAndShowData(datatrial) {
+    var editit = document.querySelector('editit');
+    datatrial.forEach(function (data) {
+        editit += /** @class */ (function () {
+            function class_1() {
+            }
+            return class_1;
+        }());
+        "col-sm" >
+            $;
+        {
+            data.title;
+        }
+        /h1>
+            < p > $;
+        {
+            data.body;
+        }
+        /p>
+            < /div>;
     });
 }
--- frontend/src/services/fetchAndShowData.ts
+++ frontend/src/services/fetchAndShowData.ts
@@ -1,30 +1,45 @@
 import datainterface from '../models/datainterface';
+const datatrial = require('.../data.json')
 
-export function fetchAndShowData() {
-   // const proxyurl = "https://cors-anywhere.herokuapp.com/";
-   // const url = `https://localhost:3000/products`
-     fetch( `localhost:4201/articles/` )
+// export function fetchAndShowData() {
+//    // const proxyurl = "https://cors-anywhere.herokuapp.com/";
+//    // const url = `https://localhost:3000/products`
+//      //fetch( `localhost:4201/articles/` )
+//     fetch(datatrial)
+//         .then(function( response : Response ) {
+//             return response.json();
+//         })
+//         .then(function( dataArray : datainterface[] ) {
+//             console.log( dataArray);
+            
+//             const tbody = <HTMLElement> document.querySelector( 'tbody' );
+            
+//             // go through workshops array and form HTML within tbody
+//             dataArray.forEach(function( dataEach ) {
+//                 // tbody.innerHTML += `<tr>
+//                 //     <td>
+//                 //         <img src="${dataEach.imageUrl}" style="width: 200px;" />
+//                 //     </td>
+//                 //     <td>${dataEach.name}</td>
+//                 //     <td>${dataEach.location.city}</td>
+//                 // </tr>`;
+//             });
+//         })
+//         .catch(function( error ) {
+//             console.log( error.message );
+//         });
+// }
 
-        .then(function( response : Response ) {
-            return response.json();
-        })
-        .then(function( dataArray : datainterface[] ) {
-            console.log( dataArray);
-            
-            const tbody = <HTMLElement> document.querySelector( 'tbody' );
-            
-            // go through workshops array and form HTML within tbody
-            dataArray.forEach(function( dataEach ) {
-                // tbody.innerHTML += `<tr>
-                //     <td>
-                //         <img src="${dataEach.imageUrl}" style="width: 200px;" />
-                //     </td>
-                //     <td>${dataEach.name}</td>
-                //     <td>${dataEach.location.city}</td>
-                // </tr>`;
-            });
-        })
-        .catch(function( error ) {
-            console.log( error.message );
-        });
+export function fetchAndShowData(datatrial : any[]) {
+
+    const editit = <HTMLElement> document.querySelector( 'editit' );
+    datatrial.forEach(function(data) {
+       editit+=  <div class="col-sm">
+
+     <h1>${data.title}</h1>
+     <p>${data.body}</p>
+
+     </div>
+    })
+
 }
